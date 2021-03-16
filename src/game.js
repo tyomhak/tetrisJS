d2c755d07114c6774b9d22e35cfd8cbaad85fb7a
gorsargs <gorsargsyanm@gmail.com>
2021-03-16 06:20:35 +0400

    moveDownEverySecond function  added with locking piece if it isn't falling down 1 second

--- src/game.js
+++ src/game.js
@@ -3,7 +3,28 @@ export default class Game {
         this.score = 0;
         this.lines = 0;
         this.level = 0;
-        this.playfield = this.createPlayField();
+        this.playfield = [
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        , [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        , [0, 0, 0, 0, 0, 0, 0, 1, 1, 1]
+    ];
         this.pieceTemplates = [
                 [
                     [0,1,0],
@@ -65,7 +86,7 @@ export default class Game {
 
     createPlayField(){
         //creating 2d array with 20 rows and 10 columns
-        
+    
         const playfield = new Array;
         for (let y = 0; y < 20; y++) {
             playfield.push(new Array);
@@ -73,9 +94,10 @@ export default class Game {
                 playfield[y].push(0);
             };
         };
+
         return playfield;
     };
-    
+
     getState(){
         //creating new playfield
         const playfield = this.createPlayField();
@@ -94,24 +116,29 @@ export default class Game {
         for (let y = 0; y < blocks.length; y++) {
             for (let x = 0; x < blocks[y].length; x++) {
                 if(blocks[y][x]){
-                    playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                    playfield[pieceY+y][pieceX+x] = blocks[y][x];
                 };
             };
         };
+
+        
         return playfield;
     };
 
     movePieceLeft(){
+        //moving piece left 
         const blocks = this.activePiece.blocks;
         this.activePiece.x -= 1;
         if (this.isPieceOutOfBounds(blocks)) this.activePiece.x += 1;
     };
     movePieceRight(){
+        //moving piece right
         const blocks = this.activePiece.blocks;
         this.activePiece.x += 1;
         if (this.isPieceOutOfBounds(blocks)) this.activePiece.x -= 1;
     };
     movePieceDown(){
+        //moving piece down
         const blocks = this.activePiece.blocks;
         this.activePiece.y += 1;
         if (this.isPieceOutOfBounds(blocks)) this.activePiece.y -= 1;
@@ -119,17 +146,24 @@ export default class Game {
     
 
     isPieceOutOfBounds(arr){
+        /*Checks positions of piece in this.playfield
+        and returns boolean true if it is out of range of playfield,false otherwise
+        */
+
         const {y : pieceY,x : pieceX, blocks} = this.activePiece;
         const playfield = this.playfield;
         for (let y = 0; y < arr.length; y++) {
             for (let x = 0; x < arr[y].length; x++) {
                 if (
                     arr[y][x] && 
-                    ((playfield[pieceY + y] === undefined || playfield[pieceY + y][pieceX + x] === undefined) || 
-                    playfield[pieceY + y][pieceX + x]))
+                    (playfield[pieceY + y] === undefined || playfield[pieceY + y][pieceX + x] === undefined))
                     {
                         return true;
-                    }
+                    };
+
+                if (arr[y][x] && (playfield[pieceY + y][pieceX + x])){
+                    return true;
+                };
             }
         }
         return false;
@@ -144,6 +178,7 @@ export default class Game {
             };
             
         };
+
     };
     rotatePiece(piece){
         let resultArray = [];
@@ -173,5 +208,23 @@ export default class Game {
         }else{
             console.log('ne povernuta')
         };
+    };
+
+    activePiecePositionCheck(){
+        let activePieceY = this.activePiece.y;
+        return activePieceY;
+    };
+
+    moveDownEverySecond() {
+        let pieceYposition = this.activePiecePositionCheck();
+        setInterval(() =>{
+            pieceYposition = this.activePiecePositionCheck();
+        }, 1000)
+        setInterval(() => {
+            this.movePieceDown();
+            if (pieceYposition == this.activePiecePositionCheck()){
+                this.lockPiece();
+            }
+        }, 1000);
     };
 };
