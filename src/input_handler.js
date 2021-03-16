ed020cbf052344671eddd45c2174c16ac64207e3
gorsargs <gorsargsyanm@gmail.com>
2021-03-16 05:05:46 +0400

    View.random function is calling under switch deleted commented piece of code

--- src/input_handler.js
+++ src/input_handler.js
@@ -9,31 +9,21 @@ export default class InputHandler {
             switch(event.keyCode) {
                 case this.left:
                     game.movePieceLeft();
-                    view.render(game.getState());
+                    
                     break;
                 case this.right:
                     game.movePieceRight();
-                    view.render(game.getState());
+                    
                     break;
                 case this.down:
                     game.movePieceDown();
-                    view.render(game.getState());
+                    
                     break;
                 case this.up:
                     game.blocksRotateToRight();
-                    view.render(game.getState());
                     break;
             };
+            view.render(game.getState());
         });
-        // document.addEventListener("keyup" , event => {
-        //     switch(event.keyCode) {
-        //         case this.left:
-        //             if(paddle.speed < 0) paddle.stop();
-        //             break;
-        //         case this.right:
-        //             if(paddle.speed > 0) paddle.stop();
-        //             break;
-        //     };
-        // });
     };
 };
