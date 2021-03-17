f92bec758616ba3c97af633a861b08f37384fdb7
gorsargs <gorsargsyanm@gmail.com>
2021-03-17 22:40:11 +0400

    random color render added

--- src/view.js
+++ src/view.js
@@ -19,15 +19,15 @@ class View{
         const i = Math.floor(Math.random() * Math.floor(colorCount));
         return this.colors[i];
     }
-    createRect (x,y,width,height,color){
+    createRect (x,y,width,height,color,lineWidth){
         this.context.fillStyle = color;
         this.context.fillRect(x * width , y * height, width, height);
-        this.context.lineWidth = 2;
+        this.context.lineWidth = lineWidth;
         this.context.strokeStyle = 'black';
         this.context.strokeRect(x * width ,y * height ,width,height);
        
     };
-    render(playfield) {
+    render(playfield,piece) {
         this.clearScreen();
         const color = this.randomColors();
         for (let y = 0; y < playfield.length; y++) {
@@ -35,11 +35,25 @@ class View{
             for (let x = 0; x < line.length; x++) {
                 const block = line[x];
                 if (block) {
-                    this.createRect(x,y,this.blockWidth,this.blockHeight,'#c44444');
-                };  
+                    this.createRect(x,y,this.blockWidth,this.blockHeight,piece,1);
+                };
             };
         };
     };
+
+    renderColor(colorField){
+        for (let y = 0; y < colorField.length; y++) {
+            const line = colorField[y];
+            for (let x = 0; x < line.length; x++) {
+                const block = line[x];
+                if (typeof(block) == 'string') {
+                    this.createRect(x,y,this.blockWidth,this.blockHeight,block,1);
+                };
+            };
+        };
+    };
+
+
     clearScreen(){
         this.context.clearRect(0,0,this.width,this.height);
     };
@@ -49,41 +63,61 @@ class View{
 
 class ContextView extends View {
 
-    randomPieceField(piece){
+    randomPieceField(piece,color){
 
+        const ctx = this.context;
+        const nextPieceGrid = [
+                                [0,0,0,0],
+                                [0,0,0,0],
+                                [0,0,0,0],
+                                [0,0,0,0],
+                            ];
         for (let y = 0; y < piece.length; y++) {
+            for (let x = 0; x < piece.length; x++) {
+                if(piece[y][x]){
+                    nextPieceGrid[y][x] = 1;
+                };  
+            };
+        };
+
+        for (let y = 0; y < nextPieceGrid.length; y++) {
             
-            for (let x = 0; x < piece[y].length; x++) {
-                const block = piece[y][x];
+            for (let x = 0; x < nextPieceGrid[y].length; x++) {
+                const block = nextPieceGrid[y][x];
                 if (block) {
-                    this.createRect(x,y,this.blockWidth,this.blockHeight,'#c44444')
+                    this.createRect(3+x,11.4+y,this.blockWidth,this.blockHeight,color)
                 };
+                if(!(block)){
+                    this.createRect(3+x,11.4+y,this.blockWidth,this.blockHeight,'white')
+                }
             }; 
         };
     };
 
 
-    RandomPieceMenu(width,height,positionY)
-        {
-            const context = this.context;
-            const positionX = this.width/2 - width/2;
-
-            context.fillStyle = '#C2DFFF';
-            context.fillRect(positionX ,positionY ,width ,height);
-            context.lineWidth = 1;
-            context.strokeStyle = 'black';
-            context.strokeRect(positionX ,positionY ,width,height);
-        };
-    TextDrawing (width,positionY,px){
-        const ctx = this.context;
+    RandomPieceMenu(width,height,positionY){   
+        this.clearScreen();
+        const context = this.context;
         const positionX = this.width/2 - width/2;
 
+        context.fillStyle = '#C2DFFF';
+        context.fillRect(positionX ,positionY ,width ,height);
+        context.lineWidth = 1;
+        context.strokeStyle = 'black';
+        context.strokeRect(positionX ,positionY ,width,height);
+    };
+    TextDrawing (text,width,positionY,px){
+        const ctx = this.context;
+        const positionX = this.width/2 - width/2;
+    
         ctx.font = `${px}px Tetris`;
         ctx.fillStyle = 'black';
         ctx.textBaseline = 'bottom';
-        ctx.fillText  ('TETRIS', positionX, positionY);
+        
+        ctx.fillText  (text, positionX, positionY);
         
     };
+
     
     
 };
