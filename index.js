e86aab324a182e55ee5c51c175aa1010877e9cc9
gorsargs <gorsargsyanm@gmail.com>
2021-03-17 11:10:03 +0400

    Context canvas added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+
 import  Game  from "./src/game.js";
-import View from "./src/view.js"
+import { View, ContextView } from "./src/view.js"
 import InputHandler from "./src/input_handler.js"
 
 const game = new Game();
 const view = new View(320,680,20,10,'gameScreen');
-
+const view2 = new ContextView(250,680,20,10,'gameScreen2');
 
 window.view = view
 window.game = game;
@@ -20,7 +21,8 @@ window.requestAnimationFrame(gameLoop);
 function gameLoop() {
     view.render(game.getState());
     window.requestAnimationFrame(gameLoop);
-
+    view2.RandomPieceMenu(200,200,200);
+    view2.TextDrawing(160,200,40);
 };
 game.moveDownEverySecond(game.speed);
 
